de1705cae976db80e72a51df804609c4a4a45bc6
nlence-l <nlence-l@student.42mulhouse.fr>
2024-12-02 20:17:37 +0100

    feat: add auth_service and clean repository

--- src/_Pong/start.sh
+++ src/_Pong/start.sh
@@ -1,4 +1,7 @@
-python ./manage.py makemigrations game --no-input
-python ./manage.py migrate
+#!/usr/bin/env bash
+
+/code/wait-for-it.sh postgres:5432 --timeout=60s -- python ./manage.py makemigrations game --no-input
+/code/wait-for-it.sh postgres:5432 --timeout=60s -- python ./manage.py migrate --no-input
+
 exec uvicorn _Pong.asgi:application --host 0.0.0.0 --port 8006
 # exec daphne -b 0.0.0.0 -p 8006 _Pong.asgi:application
---
+++ src/auth_service/start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+/code/wait-for-it.sh postgres:5432 --timeout=60s -- python ./manage.py makemigrations authentication --no-input
+/code/wait-for-it.sh postgres:5432 --timeout=60s -- python ./manage.py migrate --no-input
+
+exec gunicorn --reload --bind 0.0.0.0:8000 auth_service.wsgi:application
