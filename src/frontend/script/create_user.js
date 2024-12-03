5838569f27ca1c683687f1afb2f92dc2a7da0cf4
Adrien Gallet <agallet@k2r2p2.42mulhouse.fr>
2024-12-03 15:55:22 +0100

    small fix error

--- src/frontend/script/create_user.js
+++ src/frontend/script/create_user.js
@@ -3,7 +3,7 @@
 // Données utilisateur à envoyer
 async function sendPostRequest(){
 
-    const url = 'https://' + window.location.hostname + ':3000/api/users/';  // Remplacez par l'URL correcte de votre API
+    const url = 'https://' + window.location.hostname + ':3000/matchmaking/users/';  // Remplacez par l'URL correcte de votre API
     const userData = {
         name: "test1",
         mail: "test2@test.com",
--- src/matchmaking/start.sh
+++ src/matchmaking/start.sh
@@ -4,4 +4,6 @@
 #/code/wait-for-it.sh postgres:5432 --timeout=60s -- python ./manage.py migrate --no-input
 #/code/wait-for-it.sh postgres:5432 --timeout=60s -- exec python ./manage.py runserver 0.0.0.0:8000
 #exec gunicorn --bind 0.0.0.0:8000 api.wsgi:application #server de production on le remplacera par daphne
+python ./manage.py makemigrations Selectmode --no-input
+python ./manage.py migrate Selectmode --no-input
 exec daphne -b 0.0.0.0 -p 8000 matchmaking.asgi:application
