f82dd4c3eb34fb5031e9e8811f8bb53170c6b369
mdjemaa <mdjemaa@student.42mulhouse.fr>
2024-12-03 11:27:54 +0100

    back to health-check system in docker-compose.yml remove venv files

--- src/_Pong/start.sh
+++ src/_Pong/start.sh
@@ -1,7 +1,1 @@
-#!/usr/bin/env bash
-
-/code/wait-for-it.sh postgres:5432 --timeout=60s -- python ./manage.py makemigrations game --no-input
-/code/wait-for-it.sh postgres:5432 --timeout=60s -- python ./manage.py migrate --no-input
-
 exec uvicorn _Pong.asgi:application --host 0.0.0.0 --port 8006
-# exec daphne -b 0.0.0.0 -p 8006 _Pong.asgi:application
--- src/gateway/start.sh
+++ src/gateway/start.sh
@@ -1,8 +1,1 @@
-#!/usr/bin/env bash
-
-/code/wait-for-it.sh postgres:5432 --timeout=60s -- python ./manage.py makemigrations --no-input
-/code/wait-for-it.sh postgres:5432 --timeout=60s -- python ./manage.py migrate --no-input
-
-# exec python ./manage.py runserver 0.0.0.0:8000
 exec uvicorn gateway.asgi:application --host 0.0.0.0 --port 8057
-# exec daphne -b 0.0.0.0 -p 8006 _Pong.asgi:application
