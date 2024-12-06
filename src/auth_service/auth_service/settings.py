03740274ff0d11f303d95f3f69973752d5951013
nlence-l <nlence-l@student.42mulhouse.fr>
2024-12-06 18:24:06 +0100

    fix: users merge

--- src/auth_service/auth_service/settings.py
+++ src/auth_service/auth_service/settings.py
@@ -22,7 +22,6 @@ JWT_ALGORITHM = 'RS256'
 
 # Application definition
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
