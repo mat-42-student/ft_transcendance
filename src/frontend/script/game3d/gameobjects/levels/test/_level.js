a7509a1d1b6dfe1aa26a300210381e06a77536c3
ametzen <ametzen@student.42mulhouse.fr>
2024-12-09 15:20:55 +0100

    Trying cubemap but it isnt working

--- src/frontend/script/game3d/gameobjects/levels/test/_level.js
+++ src/frontend/script/game3d/gameobjects/levels/test/_level.js
@@ -8,59 +8,76 @@ export default class LevelTest extends LevelBase {
 	constructor() {
 		super();
 
-		throw "TODO: wip"
+		// throw "TODO: wip"
+
+		this.loadAssets();
 
 		engine.clearLevel();
 		engine.environmentScene.fog = null;
-		engine.environmentScene.background = new THREE.Color('#112211');
+		engine.environmentScene.background = this.skybox;
 
 		this.boardDiagonal = 30;
-		// const ball = new DebugBall();
-		// engine.level.add(ball);
 
-		// engine.level.add(new DebugPaddle(0));
-		// engine.level.add(new DebugPaddle(1));
+		engine.cameraTarget.diagonal = 40;
+		this.cameras = __makeCameraAngles();
 
-		// engine.level.add(new DebugScoreIndicator(0));
-		// engine.level.add(new DebugScoreIndicator(1));
+		engine.cameraTarget.teleportNow = true;
+		engine.cameraTarget.mousePositionMultiplier.set(0.1, 0.1);
+		engine.cameraTarget.mouseRotationMultiplier.set(0.1, 0.1);
+		engine.cameraTarget.smoothSpeed = 15;
 
-		// engine.cameraTarget.diagonal = 40;
-		// this.cameras = __makeCameraAngles();
-
-		// engine.cameraTarget.teleportNow = true;
-		// engine.cameraTarget.mousePositionMultiplier.set(0.1, 0.1);
-		// engine.cameraTarget.mouseRotationMultiplier.set(0.1, 0.1);
-		// engine.cameraTarget.smoothSpeed = 15;
-
-		// // global.game.focusedPlayerIndex = 'neutral';  // Forced camera choice
-
-		// engine.level.add(new DebugBoard());
+		this.addGameobjects();
 	}
 
 
 	dispose() {
 		super.dispose();
 
+		this.skybox.dispose();
 		//TODO
 	}
+
+
+	loadAssets() {
+		{
+			const loader = new THREE.CubeTextureLoader();
+
+			//FIXME this doesnt appear to do anything
+			loader.setPath( '/ressources/3d/tex/test_cubemap/',
+				()=>{console.log('Loaded')},
+				()=>{console.log('Progress')},
+				()=>{console.error('Fail')} );
+
+			this.skybox = loader.load( [
+				'px.png', 'nx.png',
+				'py.png', 'ny.png',
+				'pz.png', 'nz.png'
+			] );
+		}
+	}
+
+	addGameobjects() {
+		//TODO
+	}
+
 }
 
 
 function __makeCameraAngles() {
-	// const neutralCamera = new LevelBase.CameraStats();
+	const neutralCamera = new LevelBase.CameraStats();
 	// neutralCamera.position = new THREE.Vector3(0, 0.65, -0.4);
 	// neutralCamera.quaternion = __lookDownQuaternion(180, 60);
-	// neutralCamera.fov = 60;
+	neutralCamera.fov = 60;
 
-	// const p0Camera = new LevelBase.CameraStats();
+	const p0Camera = new LevelBase.CameraStats();
 	// p0Camera.position = new THREE.Vector3(0.6, 0.45, 0);
 	// p0Camera.quaternion = __lookDownQuaternion(90, 45);
-	// p0Camera.fov = 55;
+	p0Camera.fov = 55;
 
-	// const p1Camera = new LevelBase.CameraStats();
+	const p1Camera = new LevelBase.CameraStats();
 	// p1Camera.position.copy(p0Camera.position).x *= -1;
 	// p1Camera.quaternion = __lookDownQuaternion(-90, 45);
-	// p1Camera.fov = p0Camera.fov;
+	p1Camera.fov = p0Camera.fov;
 
 	return [p0Camera, p1Camera, neutralCamera];
 }
