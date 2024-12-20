3447b6001ed33142e8fe7c36e07fa841df7212ec
Julio <73828002+LcntJulien@users.noreply.github.com>
2024-12-20 15:31:58 +0100

    .

---
+++ src/frontend/scripts/nav.js
@@ -0,0 +1,62 @@
+import { setupHomePage, setupProfilePage } from "./pages.js";
+import { initDynamicCard } from './components/dynamic_card.js';
+import { isAuthenticated } from './api/auth.js';
+
+const routes = {
+    '#home': './partials/home.html',
+    '#profile': './partials/profile.html',
+};
+
+export function setupNavigation() {
+    window.addEventListener('popstate', (e) => {
+        e.preventDefault();
+        console.log("popstate enclanché");
+        navigateTo(window.location.hash || '#home'); // Navigate to hash or #home if hash isn't valid
+    });
+}
+
+export async function navigateTo(hash) {
+    const mainContent = document.querySelector('.main-content');
+    let route = routes[hash];
+
+    if ((!mainContent) || (!route)) return;
+
+    if (hash == '#profile') {
+        if (await isAuthenticated() == false) {
+            window.history.replaceState({}, '', `#signin`);
+            initDynamicCard('auth');
+            return;
+        } else {
+            try {
+                const response = await fetch(route);
+                const html = await response.text();
+                document.querySelector('.main-content').innerHTML = html;
+            } catch (error) {
+                console.error('Error loading page:', error);
+                return;
+            }
+            window.history.replaceState({}, '', hash);
+            setupProfilePage();
+            return;
+        }
+    } else if ((hash === '#signin' || hash === '#register') && await isAuthenticated() == true) {
+        hash = '#profile';
+        window.history.replaceState({}, '', hash);
+        setupProfilePage();
+        return;
+    } else {
+        hash = '#home';
+        route = routes[hash];
+        try {
+            const response = await fetch(route);
+            const html = await response.text();
+            document.querySelector('.main-content').innerHTML = html;
+        } catch (error) {
+            console.error('Error loading page:', error);
+            return;
+        }
+        window.history.replaceState({}, '', hash);
+        setupHomePage();
+        return;
+    }
+}
