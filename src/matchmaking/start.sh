73c211d997091ce6a79dfdf16ce973f6852eecef
mdjemaa <mdjemaa@student.42mulhouse.fr>
2025-01-23 12:24:49 +0100

    users : remove migration conflicts mathcmaking : stop in less than 11 sec

--- src/matchmaking/start.sh
+++ src/matchmaking/start.sh
@@ -3,5 +3,5 @@
 #exec gunicorn --bind 0.0.0.0:8000 api.wsgi:application #server de production on le remplacera par daphne
 python ./manage.py makemigrations Selectmode --no-input
 python ./manage.py migrate Selectmode --no-input
-python ./manage.py listenredis
+exec python ./manage.py listenredis
 #exec daphne -b 0.0.0.0 -p 8000 matchmaking.asgi:application
