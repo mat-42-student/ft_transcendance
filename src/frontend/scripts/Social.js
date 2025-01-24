2091e2d0393a2c8e3176ad109f13b9a7f96e6ab1
mdjemaa <mdjemaa@student.42mulhouse.fr>
2025-01-24 18:55:29 +0100

    remove chat/vs buttons if friend is offline

--- src/frontend/scripts/Social.js
+++ src/frontend/scripts/Social.js
@@ -9,16 +9,23 @@ export class SocialApp{
         // console.log("social incoming msg for user : " + data.user + " status " + data.status);
         let friend = state.client.friendlist.get(data.user_id);
         friend['status'] = data.status;
-        console.log(state.client.friendlist.get(data.user_id));
         this.renderFriend(data.user_id);
     }
 
     renderFriend(id) {
         const friendItem = document.querySelector(`.friend-detail[data-user-id="${id}"]`);
         if (friendItem) {
+            const status = state.client.friendlist.get(id).status
             const statusSpan = friendItem.querySelector('.friend-status');
             statusSpan.classList.remove('online', 'ingame', 'offline', 'pending');
-            statusSpan.classList.add(state.client.friendlist.get(id).status);
+            statusSpan.classList.add(status);
+            if (status === 'offline') {
+                friendItem.querySelector('.btn-match').classList.add('hidden');
+                friendItem.querySelector('.btn-chat').classList.add('hidden');
+            } else {
+                friendItem.querySelector('.btn-match').classList.remove('hidden');
+                friendItem.querySelector('.btn-chat').classList.remove('hidden');
+            }
         } else {
             console.warn(`Utilisateur avec user_id ${id} introuvable.`);
         }
