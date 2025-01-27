702f6c6ea603d3cb5b08abcd0841957701c453dd
Adrien Gallet <agallet@k1r2p5.42mulhouse.fr>
2025-01-27 13:15:29 +0100

    solved infinite while

--- src/matchmaking/Selectmode/management/commands/listenredis.py
+++ src/matchmaking/Selectmode/management/commands/listenredis.py
@@ -7,97 +7,120 @@ from asyncio import run as arun, sleep as asleep, create_task
 from signal import signal, SIGTERM, SIGINT
 
 class Command(BaseCommand):
-	help = "Commande pour écouter un canal Redis avec Pub/Sub"   
+    help = "Commande pour écouter un canal Redis avec Pub/Sub"   
 
-	def handle(self, *args, **kwargs):
-		signal(SIGINT, self.signal_handler)
-		signal(SIGTERM, self.signal_handler)
-		arun(self.main())
+    def handle(self, *args, **kwargs):
+        signal(SIGINT, self.signal_handler)
+        signal(SIGTERM, self.signal_handler)
+        arun(self.main())
 
-	async def main(self):
-		self.running = True
-		try:
-			self.redis_client = await from_url("redis://redis:6379", decode_responses=True)
-			self.pubsub = self.redis_client.pubsub(ignore_subscribe_messages=True)
-			self.channel_front = "deep_mmaking"
-			self.channel_social = "info_social"
-			print(f"Subscribing to channel: {self.channel_front}")
-			await self.pubsub.subscribe(self.channel_front)
-			await self.pubsub.subscribe(self.channel_social)
-			self.listen_task = create_task(self.listen())
-			while self.running:
-				await asleep(1)
-		except Exception as e:
-			print(e)
-		finally:
-			await self.cleanup_redis()
+    async def main(self):
+        self.running = True
+        try:
+            self.redis_client = await from_url("redis://redis:6379", decode_responses=True)
+            self.pubsub = self.redis_client.pubsub(ignore_subscribe_messages=True)
+            self.channel_front = "deep_mmaking"
+            self.channel_social = "info_social"
+            print(f"Subscribing to channel: {self.channel_front}")
+            await self.pubsub.subscribe(self.channel_front)
+            await self.pubsub.subscribe(self.channel_social)
+            self.listen_task = create_task(self.listen())
+            while self.running:
+                await asleep(1)
+        except Exception as e:
+            print(e)
+        finally:
+            await self.cleanup_redis()
 
-	async def listen(self):
-		self.user = {}
-		self.message = None
-		print("Listening for messages...")
-		async for msg in self.pubsub.listen():
-			if msg : #and msg['type'] == 'message':  # Filtre uniquement les messages réels
-				message = json.loads(msg.get('data'))
-				print(message)
-				newPlayer = Player()
+    async def listen(self):
+        self.user = {}
+        self.message = None
+        print("Listening for messages...")
+        async for msg in self.pubsub.listen():
+            if msg : #and msg['type'] == 'message':  # Filtre uniquement les messages réels
+                message = json.loads(msg.get('data'))
+                print(message)
+                newPlayer = Player()
 
-				self.parse_typeGame(message, newPlayer)
-				self.user.update(newPlayer)
+                verif = await self.verifConditions(message, newPlayer)
+                if(not verif):
+                    self.SelectTypeGame(message['body'], newPlayer)
 
-				for key, value in self.user.items():
-					print(f'Userid {key}, {value} \n')
+    
+    def signal_handler(self, sig, frame):
+        try:
+            self.listen_task.cancel()
+        except Exception as e:
+            print(e)
+        self.running = False
 
-	
-	def signal_handler(self, sig, frame):
-		try:
-			self.listen_task.cancel()
-		except Exception as e:
-			print(e)
-		self.running = False
+    async def cleanup_redis(self):
+        print("Cleaning up Redis connections...")
+        if self.pubsub:
+            await self.pubsub.unsubscribe(self.group_name)
+            await self.pubsub.close()
+        if self.redis_client:
+            await self.redis_client.close()
 
-	async def cleanup_redis(self):
-		print("Cleaning up Redis connections...")
-		if self.pubsub:
-			await self.pubsub.unsubscribe(self.group_name)
-			await self.pubsub.close()
-		if self.redis_client:
-			await self.redis_client.close()
+    async def check_statusPlayer(self, message):
+        header = message['header']
+        data = {
+            'user_id': header['id']
+        }
+        await self.redis_client.publish(self.channel_social, json.dumps(data))
+        await asleep(0.5)
+        try:
+            status = await self.redis_client.get(header['id'])
+            if (status is not None):
+                return (status)
+        except asyncio.TimeoutError:
+            print("Timeout atteint lors de l'attente de Redis.")
+            return None
 
-	async def check_statusPlayer(self, player):
-		response_timeout = 2
-		data = {
-			'user_id': player.id
-		}
-		self.redis_client.publish(self.channel_social, json.dumps(data))
-		while (response_timeout > 0):
-			status = await self.redis_client.get(player.id)
-			if (status):
-				return (status)
-			asleep(0.5)
-			reponse_timeout -= 0.5
-		return (None)
+    def setup_statusPlayer(self, message, player):
+        header = message['header']
+        data = {
+            'header':{
+                'service': 'social',
+                'dest': 'back',
+                'id': header['id'],
+            },
+            'body':{
+                'status': 'pending',
+                'status': 'test'
+            }
+        }
+        player.status = 'pending'
+        self.redis_client.publish(self.channel_front, json.dumps(data))
 
-	def setup_statusPlayer(self, player):
-		data = {
-			'header':{
-				'service': 'social',
-				'dest': 'back',
-				'id': player.id,
-			},
-			'body':{
-				'status': 'pending',
-				'status': 'test'
-			}
-		}
-		player.status = 'pending'
-		self.redis_client.pubish(self.channel_front, json.dumps(data))
+    async def verifConditions(self, message, newPlayer):
+        # Check the content of body
+        if (message.get('body') and message['body'].get('type_game')):
+            print(f'Check get body and type_game is true')
+            # Check the status of Player
+            if (await self.check_statusPlayer(message) == 'online'):
+                self.setup_statusPlayer(message, newPlayer)
+                print(f'{newPlayer}')
+                return (True)
+        return (False)
+    
+    # Research and verify conditions for the type_game selected
+    def SelectTypeGame(self, data, player):
+        print(f'SelecTypeGame -> {data}')
+        if (data.get('type_game') == '1vs1R'): # 1vs1R
+            self.user.update({player.user_id: player})
+            print(f'Player: {player}')
 
-	def parse_typeGame(self, message, newPlayer):
-		# Check the content of body
-		
-		if (message.get('body') and message['body'].get('type_game')):
-			# Check the status of Player
-			if (self.check_statusPlayer(newPlayer) == 'online'):
-				self.setup_statusPlayer(newPlayer)
-				print(f'{newPlayer}')
+        elif (data.get('type_game') == 'invite'): # Invite
+            invite = data.get['type_game']['invite']
+            if (invite.get('guest_id') is not None):
+                # Research guest_id, verify this status and send invitation
+                print(f'{invite}')
+            elif (invite.get('host_id') is not None):
+                # Research host_id to send the response by guest_id
+                print(f'{invite}')
+        elif (data.get('type_game') == 'tournament'):
+            # Research Player with opponents < 4 and the type_game is tournament
+            print(f'tournament')
+
+            
