7fb515872f6a706b410c6590c9dc9ab200b522d5
ametzen <ametzen@student.42mulhouse.fr>
2025-02-25 19:10:17 +0100

    Quit game correctly by calling state.gameApp.close()

--- src/frontend/scripts/GameBase.js
+++ src/frontend/scripts/GameBase.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import LevelBase from './game3d/gameobjects/levels/LevelBase.js';
+import { state } from './main.js';
 
 
 export class GameBase {
@@ -28,6 +29,8 @@ export class GameBase {
 	close() {
 		if (this.level)
 			this.level.dispose();
+		state.engine.scene = null;
+		state.gameApp = null;
 		this.isPlaying = false;  // just in case this object lingers somehow?
 	}
 
