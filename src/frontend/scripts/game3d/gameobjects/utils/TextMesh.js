dc2eb0c1a858b2567f983cbe7095891ea06fbe31
ametzen <ametzen@student.42mulhouse.fr>
2025-03-18 19:10:41 +0100

    Add TextMesh gameobject (Untested)

---
+++ src/frontend/scripts/game3d/gameobjects/utils/TextMesh.js
@@ -0,0 +1,64 @@
+import * as THREE from 'three';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+
+
+export default class TextMesh extends THREE.Mesh {
+
+	font = state.engine.font;
+	size = 0.08;
+	depth = 0.01;
+	curveSegments = 12;
+	bevelEnabled = false;
+	bevelThickness = 10;
+	bevelSize = 8;
+	bevelOffset = 0;
+	bevelSegments = 5;
+
+
+	/**
+	 * @param {THREE.Material} material
+	 * @param {string} text
+	 */
+	constructor(material, text = null) {
+		super(null, material);
+
+		if (text != null)
+			this.setText(text);
+	}
+
+
+	/** @param {string} newText */
+	setText(newText) {
+		if (this.#oldText == newText)
+			return;  // skip, optimization
+		this.#oldText = newText;
+
+		if (this.geometry)
+			this.geometry.dispose();
+
+		this.geometry = new TextGeometry(
+			newText,
+			{
+				font: this.font,
+				size: this.size,
+				depth: this.depth,
+				curveSegments: this.curveSegments,
+				bevelEnabled: this.bevelEnabled,
+				bevelThickness: this.bevelThickness,
+				bevelSize: this.bevelSize,
+				bevelOffset: this.bevelOffset,
+				bevelSegments: this.bevelSegments,
+			}
+		);
+	}
+
+
+	dispose() {
+		if (this.geometry) this.geometry.dispose();
+		if (this.material) this.material.dispose();
+	}
+
+
+	#oldText = null;
+
+}
