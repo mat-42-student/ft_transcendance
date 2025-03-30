a2f930a4ccf1d4f0ff8b8ca8fbbca472f7b73eeb
nlence-l <nlence-l@student.42mulhouse.fr>
2025-03-30 22:57:48 +0200

    refactor: clean ccf

--- src/chat/API_chat/management/commands/runchatworker.py
+++ src/chat/API_chat/management/commands/runchatworker.py
@@ -11,33 +11,6 @@ from redis.asyncio import Redis
 import httpx
 import jwt
 from datetime import datetime, timedelta, timezone
-
-# async def get_ccf_token():
-#     url = os.getenv('OAUTH2_CCF_TOKEN_URL')
-#     client_id = os.getenv('OAUTH2_CCF_CLIENT_ID')
-#     client_secret = os.getenv('OAUTH2_CCF_CLIENT_SECRET')
-
-#     async with httpx.AsyncClient() as client:
-#         data = {
-#             "grant_type": "client_credentials",
-#             "client_id": client_id,
-#             "client_secret": client_secret
-#         }
-#         response = await client.post(url, data=data)
-#         response.raise_for_status()
-#         token_data = response.json()
-#         return token_data['access_token'], token_data.get('expires_in', 3600)
-
-# async def get_ccf_token_cache():
-#     redis = Redis.from_url("redis://redis:6379", decode_responses=True)
-#     token = await redis.get('oauth_token')
-#     if token:
-#         return token
-#     else:
-#         new_token, expires_in = await get_ccf_token()
-#         await redis.setex('oauth_token', expires_in - 60, new_token)
-#         return new_token
-
 class Command(BaseCommand):
     help = "Listen to 'deep_chat' pub/sub redis channel"
 
@@ -105,8 +78,6 @@ class Command(BaseCommand):
 
     async def is_muted(self, exp, recipient) -> bool :
         """is exp muted by recipient ? Raises an UserNotFoundException if recipient doesnt exist"""
-
-        # token = await get_ccf_token_cache()
         
         # Generate the token
         payload = {
@@ -122,9 +93,6 @@ class Command(BaseCommand):
 
         url = f"http://users:8000/api/v1/users/{recipient}/blocks/"
         headers = {"Authorization": f"Service {token}"}
-
-        # Make the request
-        # response = requests.get(url, headers=headers)
 
         async with httpx.AsyncClient() as client:
             response = await client.get(url, headers=headers)
