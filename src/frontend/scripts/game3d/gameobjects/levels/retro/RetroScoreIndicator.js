7b8a4ed450a039be397a67e0694f899f72fb7edc
ametzen <ametzen@student.42mulhouse.fr>
2025-04-24 19:18:12 +0200

    wip score / name indicators

---
+++ src/frontend/scripts/game3d/gameobjects/levels/retro/RetroScoreIndicator.js
@@ -0,0 +1,10 @@
+import ScoreIndicator from "../../gameplay/ScoreIndicator.js";
+
+
+export default class RetroScoreIndicator extends ScoreIndicator {
+
+	onAdded() {
+		//TODO wip class, and add this to SubsceneRetroPong
+	}
+
+}
--- src/frontend/scripts/game3d/gameobjects/levels/retro/SubsceneRetroPong.js
+++ src/frontend/scripts/game3d/gameobjects/levels/retro/SubsceneRetroPong.js
@@ -16,6 +16,25 @@ export default class SubsceneRetroPong extends THREE.Scene {
 		this.add(new THREE.AmbientLight("#ff00ff", 1));  // just in case i accidentally have a lit material
 
 		this.whiteMaterial = new THREE.MeshBasicMaterial({color: "#ffffff"});
+		this.grayMaterial = new THREE.MeshBasicMaterial({color: "#aaaaaa"});
+
+		this.scoreText = [ new TextMesh(this.grayMaterial), new TextMesh(this.grayMaterial) ];
+		this.scoreText.forEach((t, i) => {
+			this.add(t);
+			t.size = 0.2;
+			t.depth = 0;
+			t.position.set(i ? -0.5 : 0.5, 0.5, -0.1);
+			t.setText(`?${i}`);
+		});
+
+		this.namesText = [ new TextMesh(this.grayMaterial), new TextMesh(this.grayMaterial), ];
+		this.namesText.forEach((t, i) => {
+			this.add(t);
+			t.size = 0.1;
+			t.depth = 0;
+			t.position.set(i ? -0.5 : 0.5, -0.8, -0.1);
+			t.setText(`?${i}`);
+		});
 	}
 
 	onFrame(delta, time) {
@@ -23,6 +42,7 @@ export default class SubsceneRetroPong extends THREE.Scene {
 
 	dispose() {
 		if (this.whiteMaterial) this.whiteMaterial.dispose();
+		if (this.grayMaterial) this.grayMaterial.dispose();
 	}
 
 }
