b13fbc0099934a956b35458deff8cf8c43f00d49
nlence-l <nlence-l@student.42mulhouse.fr>
2025-05-01 19:55:50 +0200

    feat: clean url after redirection

--- src/auth/entrypoint.sh
+++ src/auth/entrypoint.sh
@@ -2,7 +2,6 @@
 set -e  # Exit immediately if any command fails
 
 # Run Django setup commands
-python manage.py collectstatic --noinput
 python manage.py makemigrations authentication --noinput
 python manage.py migrate --noinput
 
